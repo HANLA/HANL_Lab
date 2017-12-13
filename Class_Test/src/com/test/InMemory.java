fcf9867de1ab8ca7857876de127086b6caa4355f
HAN'LA <dackhydra@hotmail.com>
2017-12-13 10:47:44 +0900

    class test

---
+++ Class_Test/src/com/test/InMemory.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class InMemory
+{
+	public String[] ID = new String[MaxArrayCapacity];
+
+	public String[] PW = new String[MaxArrayCapacity];
+
+	public int LastIndex = 0;
+
+	public static final int MaxArrayCapacity = 100;
+}
